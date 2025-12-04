b0259f35236ae82179ac9599c6556b6e81c6f8ea
vincyLim <lim.vincy188@1utar.my>
2025-12-04 19:49:27 +0800

    Fix for auth for deploy

--- proxy.ts
+++ proxy.ts
@@ -6,5 +6,6 @@ export default NextAuth(authConfig).auth;
  
 export const config = {
   // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  //matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/((?!api/auth|_next/static|_next/image|.*\\.png$).*)'],
 };
